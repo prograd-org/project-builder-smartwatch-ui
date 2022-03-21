9cc9378d3f33fb2e02dceeef96d8472a1abf7891
Sparsh Pathak <54447950+spathak98@users.noreply.github.com>
2022-03-21 12:18:44 +0530

    file updated

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var everyday = new Date();
-var hrs = everyday.getHours();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var setTime = setInterval(showtime, 1000);
 
-if(hrs<10){
-    hrs=`0${hrs}`;
+
+function showtime()
+{
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var day = today.getDay();
+
+
+    if(hours<10)
+    {
+        hours= `0${hours}`;
+    }
+
+    if(minutes<10)
+    {
+        minutes=`0${minutes}`;
+
+    }
+
+       toptime.innerHTML=hours+":"+ minutes;
+       middletime.innerHTML= hours+":"+minutes;
+       daydisplay.innerHTML= days[day];
 }
 
-var min = everyday.getMinutes();
-if(min<10){
-    min=`0${min}`;
-}
-
-var time = hrs + " : " + min;
-document.getElementById('firsttime').innerHTML = time;
-document.getElementById('secondtime').innerHTML = time;
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var daysofweek = days[new Date().getDay()];
-
-document.getElementById('day').innerHTML = daysofweek;
+      setInterval(time,1000);
